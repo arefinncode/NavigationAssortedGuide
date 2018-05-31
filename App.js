5f82350b8daa86a3aa2bf6e97d7ea206d458db99
arefin <sultanul.arefin@nordecode.com>
2018-05-31 14:18:59 +0600

    icons and createBottomTabNavigator

--- App.js
+++ App.js
@@ -17,6 +17,9 @@ import {
 import { AppRegistry, Image } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 
+// import {createMaterialTopTabNavigator} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 // type Props = {};
 class HomeScreen extends React.Component {
     render() {
@@ -40,15 +43,54 @@ class SettingsScreen extends React.Component {
 
 
 
-const RootStack =  createBottomTabNavigator({
+
+// createMaterialTopTabNavigator
+// createBottomTabNavigator
+
+const RootStack=createBottomTabNavigator(
+    {
+        Home: HomeScreen,
+        Settings: SettingsScreen,
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Settings') {
+                    iconName = `ios-options${focused ? '' : '-outline'}`;
+                }
+
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    }
+);
+
+/*
+const RootStack =  createBottomTabNavigator(
+    {
+
 // export default class App extends Component ({
     Home: HomeScreen,
     Settings: SettingsScreen,
 });
 
+*/
+
+
+
 export default class App extends React.Component {
     render() {
-        return <RootStack />;
+        return <RootStack/>;
     }
 }
 
@@ -58,16 +100,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
     }
-
 });
